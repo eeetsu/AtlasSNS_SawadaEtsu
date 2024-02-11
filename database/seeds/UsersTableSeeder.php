0bbdc6afce00a438430a9cdad6af8d48704130bb
etsu <>
2024-02-11 17:23:36 +0900

    “シードファイルTRY！！！”

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,22 +12,26 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
       DB::table('users')->insert([
-        ['username' => 'Atlas一郎'],
-        ['user_id ' => '1',
-        'mail' => 'atlas_ichi@com',
-        'password' => bcrypt('123456')],
-        ['username' => 'Atlas二郎'],
-        ['user_id ' => '2',
-        'mail' => 'atlas_ni@com',
-        'password' => bcrypt('234567')],
-        ['username' => 'Atlas三郎'],
-        ['user_id ' => '3',
-        'mail' => 'atlas_san@com',
-        'password' => bcrypt('345678')],
-        ['username' => 'Atlas四郎'],
-        ['user_id ' => '4',
-        'mail' => 'atlas_yon@com',
-        'password' => bcrypt('456789')],
+        [
+          'username' => 'Atlas一郎',
+          'mail' => 'atlas_ichi@com',
+          'password' => bcrypt('123456')
+        ],
+        [
+          'username' => 'Atlas二郎',
+          'mail' => 'atlas_ni@com',
+          'password' => bcrypt('234567')
+        ],
+        [
+          'username' => 'Atlas三郎',
+          'mail' => 'atlas_san@com',
+          'password' => bcrypt('345678')
+        ],
+        [
+          'username' => 'Atlas四郎',
+          'mail' => 'atlas_yon@com',
+          'password' => bcrypt('456789')
+        ],
       ]);
     }
 
