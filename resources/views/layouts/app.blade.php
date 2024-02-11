6d7fbba8e3220e393a236152668ee511b13de0a4
etsu <>
2024-02-12 01:47:37 +0900

    “app.blade.phpの作成！”

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        @yield('content')
+    </div>
+</body>
+</html>
