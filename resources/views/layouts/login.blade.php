c76c181b53812de6a24d96338abf45d3edfed275
etsu <>
2024-02-16 03:33:47 +0900

    “【ヘッダー】Atlasロゴにトップページへ遷移するリンクを設置する”

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -22,10 +22,10 @@
 <body>
     <header>
         <div id = "head">
-        <h1><a><img src="images/logo.png"></a></h1>
+        <h1><a><img src="images/atlas.png"></a></h1>
             <div id="">
                 <div id="">
-                    <p>〇〇さん<img src="images/arrow.png"></p>
+                    <p>{{ Auth::user()->username }}さん<img src="images/arrow.png"></p>
                 <div>
                 <ul>
                     <li><a href="/top">ホーム</a></li>
@@ -41,7 +41,7 @@
         </div >
         <div id="side-bar">
             <div id="confirm">
-                <p>〇〇さんの</p>
+                <p> {{ Auth::user()->username }}さん</p>
                 <div>
                 <p>フォロー数</p>
                 <p>〇〇名</p>
