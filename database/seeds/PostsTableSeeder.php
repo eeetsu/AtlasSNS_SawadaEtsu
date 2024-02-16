f0f706303e2676332b2ca4e645a3698c41cf2879
etsu <>
2024-02-17 00:23:38 +0900

    “PostsTableSeeder.FollowsTableSeeder作成”

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+     public function run()
+    {
+      DB::table('posts')->insert([
+        [
+          'user_id' => '1',
+          'post' => 'おはよう！',
+        ],
+        [
+          'user_id' => '2',
+          'post' => 'こんにちは！',
+        ],
+        [
+          'user_id' => '3',
+          'post' => 'こんばんは！',
+        ],
+        [
+          'user_id' => '4',
+          'post' => 'また明日！',
+        ],
+      ]);
+    }
+
+}
