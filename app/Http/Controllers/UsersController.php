2d96175f209493da12824d0e9acfbd6bc86ad228
eeetsu <gajumaru375@gmail.com>
2024-02-18 20:46:35 +0900

    “検索機能”

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
@@ -14,13 +15,20 @@ class UsersController extends Controller
     } else {
         return redirect()->route('login');
     }
-}
-public function search()
-{
-    if (Auth::check()) {
-        return view('users.search');
-    } else {
-        return redirect()->route('login');
     }
-}
+    public function search(Request $request)    //検索機能
+    {
+     // 1つ目の処理
+      $keyword = $request->input('keyword');
+     // 2つ目の処理
+      if(!empty($keyword)){
+      $users = User::where('username','like', '%'.$keyword.'%')->where('id', '!=', Auth::user()->id)->get();
+      }else{
+            $users = User::where('id', '!=', Auth::user()->id)->get();
+            //検索時、Authユーザーが表示されないようにする
+      }
+      // 3つ目の処理
+      return view('users.search',['users'=>$users, 'keyword'=>$keyword]);
+    }
+
 }
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -2,6 +2,20 @@
 
 @section('content')
 
+<div class="container">
+<!-- 検索フォームへ飛ぶための追加ボタンを設置！ -->
+    <form action="/search" method="get">
+        @csrf
+        <input type="text" name="keyword" class="form" placeholder="ユーザー名" value="{{ $keyword ?? '' }}">
+        <button type="submit" class="btn btn-success">検索ロゴ</button>
+    </form>
 
+    <div class="user-icons">
+        @foreach($users as $user)
+            <img src="{{ $user->images }}" alt="user-icon">
+            <span>{{ $user->username }}</span>
+        @endforeach
+    </div>
+</div>
 
 @endsection
