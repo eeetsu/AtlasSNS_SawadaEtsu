81223f140601873b6fc4f46f0fbb5ff46625d4f8
eeetsu <gajumaru375@gmail.com>
2024-03-26 14:08:01 +0900

    “ログイン登録画面”

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,25 +14,27 @@
 
 <!-- 適切なURLを入力してください -->
 {!! Form::open(['url' => '/register', 'method' => 'POST']) !!}
+<div class="logout-form">
+<h2 class="atlas">New user registration</h2>
 
-<h2>新規ユーザー登録</h2>
-
-{{ Form::label('ユーザー名') }}
+<div class="back-color">
+<p class="p-label">user name</p>
 {{ Form::text('username',null,['class' => 'input', 'name' => 'username']) }}
 
-{{ Form::label('メールアドレス') }}
+<p class="p-label">meil address</p>
 {{ Form::text('mail',null,['class' => 'input', 'name' => 'mail']) }}
 
-{{ Form::label('パスワード') }}
+<p class="p-label">password</p>
 {{ Form::text('password',null,['class' => 'input', 'name' => 'password']) }}
 
-{{ Form::label('パスワード確認') }}
+<p class="p-label">password confirmation</p>
 {{ Form::text('password_confirmation',null,['class' => 'input', 'name' => 'password_confirmation']) }}
 
-{{ Form::submit('登録') }}
+{{ Form::submit('Registration',['class' => 'btn btn-danger']) }}
 
 <p><a href="/login">ログイン画面へ戻る</a></p>
-
+</div>
+</div>
 {!! Form::close() !!}
 
 
